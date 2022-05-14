246c5066eee215b2c07699df5c6005854e686845
Sebastian Krieter <sebastian.krieter@ovgu.de>
2022-05-14 15:18:19 +0200

    Updates script for building copyright text

--- add_copyright_to_all.py
+++ add_copyright_to_all.py
@@ -2,43 +2,64 @@ import sys
 import os
 from pathlib import Path
 import yaml
+import xml.etree.ElementTree as xml
 
 root_path = Path(".")
+root_information_path = root_path.joinpath("copyright_config.yml")
+
+with open(root_information_path, "r") as ymlfile:
+	root_information = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
 
 def add_to_project(project_path):
+	print(str(project_path))
+	pom_path = project_path.joinpath("pom.xml")
 	config_path = project_path.joinpath("copyright_config.yml")
 	src_path = project_path.joinpath("src")
 
-	print("Config Path: " + str(config_path))
-	print("Src Path: " + str(src_path))
+	if config_path.exists():
+		with open(config_path, "r") as ymlfile:
+			config = yaml.load(ymlfile, Loader=yaml.FullLoader)
+			information = root_information | config
+	else:
+		information = root_information
 
-	with open(config_path, "r") as ymlfile:
-		config = yaml.load(ymlfile, Loader=yaml.FullLoader)
+	root = xml.parse(pom_path).getroot()
+	
+	information['name'] = pom_value(root, 'name')
+	information['description'] = pom_value(root, 'description')
+	information['url'] = pom_value(root, 'url')
 
-	config_section_license = config["license"]
-	config_section_program = config["program"]
+	copyright_text = create_copyright_text(information)
+	modify_source_files(src_path, copyright_text)
 
-	license_name = config_section_license["name"]
-	license_version = config_section_license["version"]
-	program_name = config_section_program["name"]
-	program_description = config_section_program["description"]
-	program_authors = config_section_program["authors"]
-	program_url = config_section_program["url"]
-	copyright_year = config_section_program["year"]
 
-	with open("copyright-" + license_name, "r") as f:
+def pom_value(root, name):
+	maven_prefix = '{http://maven.apache.org/POM/4.0.0}'
+	element = root.find(maven_prefix + name)
+	if element is None:
+		return ''
+	else:
+		return element.text
+
+
+def create_copyright_text(information):
+	print("Copyright Text:")
+	with open("copyright-" + information['license_name'], "r") as f:
 		copyright_text = f.read()
 		copyright_text = copyright_text.format(
-			version = license_version,
-			program = program_name,
-			description = program_description,
-			authors = program_authors,
-			url= program_url,
-			year = copyright_year
+			version = information['license_version'],
+			program = information['name'],
+			description = information['description'],
+			authors = information['authors'],
+			url= information['url'],
+			year = information['year']
 		)
-	print("Copyright Text")
 	print(copyright_text)
+	return copyright_text
 
+
+def modify_source_files(src_path, copyright_text):
 	print("Files:")
 	for (dir_path, dir_names, file_names) in os.walk(src_path):
 		for file_name in file_names:
@@ -69,6 +90,7 @@ def write_java_file(file_name, original_lines, copyright_text):
 			if start:
 				f.write(line)
 
-dirs =[d for d in root_path.iterdir() if d.is_dir() and d.joinpath("copyright_config.yml").exists()]
+
+dirs =[d for d in root_path.iterdir() if d.is_dir() and d.joinpath("pom.xml").exists() and d.joinpath('src').exists()]
 for d in dirs:
 	add_to_project(d)
