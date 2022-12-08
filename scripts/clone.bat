8193f8d25d9fc06e511aaecc2a625e41394c4089
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2022-12-08 19:29:15 +0100

    Update clone script

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -1,6 +1,6 @@
 :<<BATCH
     @echo off
-    for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch v0.1.0 %%A
+    for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch stable-0.1.0 %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress --branch v0.1.0 < scripts/repo.txt
+xargs -L1 git clone --progress --branch stable-0.1.0 < scripts/repo.txt
