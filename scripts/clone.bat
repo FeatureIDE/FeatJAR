6ab78694cc43c449a21627fac6053aa4f062c09a
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-02-10 14:16:18 +0100

    Adapts clone script

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -3,4 +3,4 @@
     for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch stable-0.1.0 %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress --branch stable-0.1.0 < scripts/repo.txt
+xargs -L1 git clone --progress --branch evaluation_sampling_algorithms < scripts/repo.txt
