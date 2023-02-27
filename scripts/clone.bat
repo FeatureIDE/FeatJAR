1bf61ae5f4e51c909bcffcce7cfea5027b137951
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-02-27 16:54:40 +0100

    Changes clone script

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -1,6 +1,6 @@
 :<<BATCH
     @echo off
-    for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch stable-0.1.0 %%A
+    for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch evaluation_continuous_sampling %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress --branch evaluation_sampling_algorithms < scripts/repo.txt
+xargs -L1 git clone --progress --branch evaluation_continuous_sampling < scripts/repo.txt
