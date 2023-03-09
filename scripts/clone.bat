104e709aa3de8aa94ab7bd49e49bfb06e8875e7f
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-03-09 19:51:43 +0100

    Prepares clone script for eval

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -1,6 +1,6 @@
 :<<BATCH
     @echo off
-    for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch stable-0.1.0 %%A
+for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch evaluation_interaction_analysis %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress --branch evaluation_sampling_algorithms < scripts/repo.txt
+xargs -L1 git clone --progress --branch evaluation_interaction_analysis < scripts/repo.txt
