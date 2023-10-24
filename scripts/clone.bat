d7fbd178beff88958f4b0b9ab9659854b83b2e0b
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-10-24 03:20:58 +0200

    Updates clone script

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -1,6 +1,6 @@
 :<<BATCH
     @echo off
-    for /F "tokens=*" %%A in (scripts/repo.txt) do git clone %%A
+for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch evaluation_interaction_analysis2 %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress < scripts/repo.txt
+xargs -L1 git clone --progress --branch evaluation_interaction_analysis2 < scripts/repo.txt
