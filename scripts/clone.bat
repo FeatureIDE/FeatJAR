ada22410d4d3a30e44091bf57c79569dc5f811f4
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2024-03-12 13:12:23 +0100

    Fixes branch in clone script

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -1,6 +1,6 @@
 :<<BATCH
     @echo off
-for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch evaluation_interaction_analysis2 %%A
+for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch main %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress --branch evaluation_interaction_analysis2 < scripts/repo.txt
+xargs -L1 git clone --progress --branch main < scripts/repo.txt
