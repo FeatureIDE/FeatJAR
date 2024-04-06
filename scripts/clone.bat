433d242b2ef9c1c951910cc25ccd0da2fbb63dd9
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2024-04-06 21:10:37 +0200

    Adapts clone script
    Signed-off-by Sebastian Krieter <sebastian.krieter@uni-ulm.de>

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -1,6 +1,6 @@
 :<<BATCH
     @echo off
-for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch main %%A
+for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch evaluation_sample_reducer %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress --branch main < scripts/repo.txt
+xargs -L1 git clone --progress --branch evaluation_sample_reducer < scripts/repo.txt
