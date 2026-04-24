4d28643e30334d7048721f08ae9ca0cd30c8f016
Klara Surmeier <k.surmeier@tu-braunschweig.de>
2026-04-24 11:15:41 +0200

    StringLength: new aggregate function for string variables

---
+++ src/main/java/de/featjar/formula/structure/term/function/string/StringLength.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2025 FeatJAR-Development-Team
+ *
+ * This file is part of FeatJAR-formula.
+ *
+ * formula is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * formula is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with formula. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See <https://github.com/FeatureIDE/FeatJAR-formula> for further information.
+ */
+
+package de.featjar.formula.structure.term.function.string;
+
+import java.util.List;
+import java.util.Optional;
+
+import de.featjar.formula.structure.ANonTerminalExpression;
+import de.featjar.formula.structure.IExpression;
+import de.featjar.formula.structure.IUnaryExpression;
+import de.featjar.formula.structure.term.ITerm;
+import de.featjar.formula.structure.term.function.IFunction;
+
+public class StringLength extends ANonTerminalExpression implements IFunction, IUnaryExpression {
+	
+	protected StringLength() {}
+
+    public StringLength(ITerm variable) {
+        super(variable);
+    }
+
+    @Override
+    public String getName() {
+        return "len";
+    }
+    
+    @Override
+    public Class<Long> getType() {
+        return Long.class;
+    }
+
+    @Override
+    public Class<String> getChildrenType() {
+        return String.class;
+    }
+
+    @Override
+    public Optional<Integer> evaluate(List<?> values) {
+    	IExpression variable = this.getChildren().get(0);
+    	Optional<String> value = (Optional<String>) variable.evaluate(values);
+        return Optional.ofNullable(value.orElseThrow().length());
+    }
+
+    @Override
+    public StringLength cloneNode() {
+        return new StringLength();
+    }
+}
+
