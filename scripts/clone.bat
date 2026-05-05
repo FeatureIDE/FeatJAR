f03c174930825ea85a28093a3c601394b01b09bc
Sebastian Krieter <sebastian.krieter@tu-braunschweig.de>
2026-05-05 16:14:05 +0200

    Removes unnecessary scripts

--- scripts/clone.bat
+++
@@ -1,6 +0,0 @@
-:<<BATCH
-    @echo off
-for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch main %%A
-    exit /b
-BATCH
-xargs -L1 git clone --progress --branch main < scripts/repo.txt
