f868c3aca084fa22c1aab07f1ca3f69955f385ef
Slwan Jradat <slwanjaradat870@gmail.com>
2026-09-15 15:05:25 +0200

    test: split FeatJARWrapper into focused tests Fixes #2

--- feature-model-assistance/src/test/java/de/featjar/featureide/FeatJARWrapperTest.java
+++ feature-model-assistance/src/test/java/de/featjar/featureide/FeatJARWrapperTest.java
@@ -20,9 +20,14 @@
  */
 package de.featjar.featureide;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import de.featjar.Common;
 import de.featjar.base.data.Void;
@@ -34,118 +39,172 @@ import de.featjar.feature.model.io.uvl.UVLFeatureModelFormat;
 import de.featjar.formula.structure.connective.And;
 import de.featjar.formula.structure.connective.Implies;
 import de.featjar.formula.structure.connective.Not;
-import java.math.BigInteger;
-import java.util.Arrays;
-import org.junit.jupiter.api.Test;
 
 public class FeatJARWrapperTest {
+        private IFeature root, alternative, and, or, remove, removeremoveA, removeremoveB, removeremoveAremoveA, removeremoveBremoveA,
+            altA, altB, altC, andMandatoryA, andOptionalB, andMandatoryAOptionalA, orA, orB, orC;
 
-    @SuppressWarnings("unused")
-    @Test
-    public void featjarWrapperWorksCorrectly() {
-        FeatJARWrapper featJARWrapper = new FeatJARWrapper();
+        private IConstraint removeConstraint1;
+        private IConstraint removeConstraint2;
 
-        FeatureModelBuilder featureModelBuilder = featJARWrapper.featureModelBuilder();
-        IFeature root = featureModelBuilder.addRoot("root");
-        IFeature alternative = featureModelBuilder.addFeatureBelow("alternative", root);
-        IFeature and = featureModelBuilder.addFeatureBelow("and", root);
-        IFeature or = featureModelBuilder.addFeatureBelow("or", root);
+        private FeatureModelBuilder featureModelBuilder;
+        private FeatJARWrapper featJARWrapper;
 
-        IFeature remove = featureModelBuilder.addFeatureBelow("remove", root);
-        IFeature removeremoveA = featureModelBuilder.addFeatureBelow("removeremoveA", remove);
-        IFeature removeremoveB = featureModelBuilder.addFeatureBelow("removeremoveB", remove);
-        IFeature removeremoveAremoveA = featureModelBuilder.addFeatureBelow("removeremoveAremoveA", removeremoveA);
-        IFeature removeremoveBremoveA = featureModelBuilder.addFeatureBelow("removeremoveBremoveA", removeremoveB);
+        private FeatureModelAnalyzer analyzer;
 
-        IFeature altA = featureModelBuilder.addFeatureBelow("altA", alternative);
-        IFeature altB = featureModelBuilder.addFeatureBelow("altB", alternative);
-        IFeature altC = featureModelBuilder.addFeatureBelow("altC", alternative);
+        @SuppressWarnings("unused")
+        @BeforeEach
+        public void initializeFeatureModelToTest(){
+                featJARWrapper = new FeatJARWrapper();
+                featureModelBuilder = featJARWrapper.featureModelBuilder();
 
-        IFeature andMandatoryA = featureModelBuilder.addFeatureBelow("andMandatoryA", and);
-        IFeature andOptionalB = featureModelBuilder.addFeatureBelow("andOptionalB", and);
-        IFeature andMandatoryAOptionalA = featureModelBuilder.addFeatureBelow("andMandatoryAOptionalA", andMandatoryA);
+                root = featureModelBuilder.addRoot("root");
+                alternative = featureModelBuilder.addFeatureBelow("alternative", root);
+                and = featureModelBuilder.addFeatureBelow("and", root);
+                or = featureModelBuilder.addFeatureBelow("or", root);
 
-        IFeature orA = featureModelBuilder.addFeatureBelow("orA", or);
-        IFeature orB = featureModelBuilder.addFeatureBelow("orB", or);
-        IFeature orC = featureModelBuilder.addFeatureBelow("orC", or);
+                remove = featureModelBuilder.addFeatureBelow("remove", root);
+                removeremoveA = featureModelBuilder.addFeatureBelow("removeremoveA", remove);
+                removeremoveB = featureModelBuilder.addFeatureBelow("removeremoveB", remove);
+                removeremoveAremoveA = featureModelBuilder.addFeatureBelow("removeremoveAremoveA", removeremoveA);
+                removeremoveBremoveA = featureModelBuilder.addFeatureBelow("removeremoveBremoveA", removeremoveB);
 
-        featureModelBuilder.setGroupFeaturesIsInToAlternative(altA);
-        featureModelBuilder.setGroupFeaturesIsInToAnd(andMandatoryA);
-        featureModelBuilder.setGroupFeaturesIsInToOr(orA);
+                altA = featureModelBuilder.addFeatureBelow("altA", alternative);
+                altB = featureModelBuilder.addFeatureBelow("altB", alternative);
+                altC = featureModelBuilder.addFeatureBelow("altC", alternative);
 
-        featureModelBuilder.setFeatureToMandatory(andMandatoryA);
-        featureModelBuilder.setFeatureToOptional(andOptionalB);
-        featureModelBuilder.setFeatureToOptional(andMandatoryAOptionalA);
+                andMandatoryA = featureModelBuilder.addFeatureBelow("andMandatoryA", and);
+                andOptionalB = featureModelBuilder.addFeatureBelow("andOptionalB", and);
+                andMandatoryAOptionalA = featureModelBuilder.addFeatureBelow("andMandatoryAOptionalA", andMandatoryA);
 
-        featureModelBuilder.addConstraint(new And(
-                new Not(featureModelBuilder.createLiteral(altC)),
-                new Not(featureModelBuilder.createLiteral(andMandatoryAOptionalA))));
+                orA = featureModelBuilder.addFeatureBelow("orA", or);
+                orB = featureModelBuilder.addFeatureBelow("orB", or);
+                orC = featureModelBuilder.addFeatureBelow("orC", or);
 
-        featureModelBuilder.addConstraint(
-                new Implies(featureModelBuilder.createLiteral(orB), featureModelBuilder.createLiteral(orC)));
+                featureModelBuilder.setGroupFeaturesIsInToAlternative(altA);
+                featureModelBuilder.setGroupFeaturesIsInToAnd(andMandatoryA);
+                featureModelBuilder.setGroupFeaturesIsInToOr(orA);
 
-        IConstraint removeConstraint1 =
-                featureModelBuilder.addConstraint(new Not(featureModelBuilder.createLiteral(root)));
-        IConstraint removeConstraint2 =
-                featureModelBuilder.addConstraint(featureModelBuilder.createLiteral(removeremoveBremoveA));
+                featureModelBuilder.setFeatureToMandatory(andMandatoryA);
+                featureModelBuilder.setFeatureToOptional(andOptionalB);
+                featureModelBuilder.setFeatureToOptional(andMandatoryAOptionalA);
 
-        assertFalse(featureModelBuilder.removeFeatureTree(remove));
+                featureModelBuilder.addConstraint(new And(
+                        new Not(featureModelBuilder.createLiteral(altC)),
+                        new Not(featureModelBuilder.createLiteral(andMandatoryAOptionalA))));
 
-        featureModelBuilder.removeConstraint(removeConstraint2);
-        featureModelBuilder.removeConstraint(removeConstraint1);
+                featureModelBuilder.addConstraint(
+                        new Implies(featureModelBuilder.createLiteral(orB), featureModelBuilder.createLiteral(orC)));
 
-        assertTrue(featureModelBuilder.removeFeature(removeremoveA));
-        assertTrue(featureModelBuilder.removeFeatureTree(remove));
+                removeConstraint1 =
+                        featureModelBuilder.addConstraint(new Not(featureModelBuilder.createLiteral(root)));
+                removeConstraint2 =
+                        featureModelBuilder.addConstraint(featureModelBuilder.createLiteral(removeremoveBremoveA));
 
-        assertEquals(
-                Common.load("test_model.uvl", new StringTextFormat()),
-                new UVLFeatureModelFormat()
-                        .serialize(featureModelBuilder.getFeatureModel())
-                        .get());
+                analyzer = featJARWrapper.featureModelAnalyzer(featureModelBuilder.getFeatureModel());
 
-        FeatureModelAnalyzer analyzer = featJARWrapper.featureModelAnalyzer(featureModelBuilder.getFeatureModel());
+        }
 
-        assertEquals(
+        @SuppressWarnings("unused")
+        @Test
+        public void doesNotRemoveFeatureTreeWhenReferencedByConstraint(){
+                assertFalse(featureModelBuilder.removeFeatureTree(remove));
+        }
+
+        @SuppressWarnings("unused")
+        @Test
+        public void doesRemoveFeatureWhenReferencedByConstraint(){
+                featureModelBuilder.removeConstraint(removeConstraint2);
+                featureModelBuilder.removeConstraint(removeConstraint1);
+
+                assertTrue(featureModelBuilder.removeFeature(removeremoveA)); //
+                assertTrue(featureModelBuilder.removeFeatureTree(remove));
+        }
+
+        @SuppressWarnings("unused")
+        @Test
+        public void serializeFeatureModel(){
+                removeTemporaryConstraintsAndSubtree();
+
+                assertEquals(
+                        Common.load("test_model.uvl", new StringTextFormat()),
+                        new UVLFeatureModelFormat()
+                                .serialize(featureModelBuilder.getFeatureModel())
+                                .get());
+        }
+
+        @SuppressWarnings("unused")
+        @Test
+        public void doFeaturesExist(){
+                removeTemporaryConstraintsAndSubtree();
+
+                assertEquals(
                 Arrays.asList(altA, altC, orB, orC), analyzer.toFeature(Arrays.asList("altA", "altC", "orB", "orC")));
-        assertEquals(root, analyzer.toFeature("root"));
+                assertEquals(root, analyzer.toFeature("root"));
+        }
 
-        analyzer.toFormula().orElseThrow();
-        analyzer.toCNF().orElseThrow();
-        analyzer.toClauseList().orElseThrow();
-        analyzer.toSat4JSolver().orElseThrow();
+        @SuppressWarnings("unused")
+        @Test
+        public void analyzerTest(){
+                removeTemporaryConstraintsAndSubtree();
 
-        DataTree<Void> statistics = analyzer.statistics();
-        assertEquals(
-                4, statistics.getChild(0).get().getChild(0).get().getValue().get());
-        assertEquals(
-                2.4, statistics.getChild(0).get().getChild(1).get().getValue().get());
-        assertEquals(
-                8L, statistics.getChild(0).get().getChild(2).get().getValue().get());
-        assertEquals(
-                3L, statistics.getChild(0).get().getChild(3).get().getValue().get());
-        assertEquals(
-                5L, statistics.getChild(0).get().getChild(4).get().getValue().get());
-        assertEquals(
-                4L, statistics.getChild(1).get().getChild(0).get().getValue().get());
-        assertEquals(
-                4L, statistics.getChild(1).get().getChild(1).get().getValue().get());
-        assertEquals(
-                4L, statistics.getChild(1).get().getChild(2).get().getValue().get());
+                analyzer.toFormula().orElseThrow();
+                analyzer.toCNF().orElseThrow();
+                analyzer.toClauseList().orElseThrow();
+                analyzer.toSat4JSolver().orElseThrow();
+        }
 
-        assertTrue(analyzer.isSatisfiable().orElseThrow());
-        assertFalse(analyzer.isVoid().orElseThrow());
-        assertEquals(Arrays.asList("root"), analyzer.core().orElseThrow());
-        assertEquals(
-                Arrays.asList("altC", "andMandatoryAOptionalA"), analyzer.dead().orElseThrow());
-        assertEquals(
-                BigInteger.valueOf(54), analyzer.numberOfValidConfigurations().orElseThrow());
-        analyzer.atomicSets().orElseThrow();
+        @SuppressWarnings("unused")
+        @Test  
+        public void analyzerStatisticsTest(){
+                removeTemporaryConstraintsAndSubtree();
 
-        analyzer.project(Arrays.asList("altA", "altC", "orB", "orC")).orElseThrow();
-        analyzer.slice(Arrays.asList("altA", "altC", "orB", "orC")).orElseThrow();
+                 DataTree<Void> statistics = analyzer.statistics();
+                assertEquals(
+                        4, statistics.getChild(0).get().getChild(0).get().getValue().get());
+                assertEquals(
+                        2.4, statistics.getChild(0).get().getChild(1).get().getValue().get());
+                assertEquals(
+                        8L, statistics.getChild(0).get().getChild(2).get().getValue().get());
+                assertEquals(
+                        3L, statistics.getChild(0).get().getChild(3).get().getValue().get());
+                assertEquals(
+                        5L, statistics.getChild(0).get().getChild(4).get().getValue().get());
+                assertEquals(
+                        4L, statistics.getChild(1).get().getChild(0).get().getValue().get());
+                assertEquals(
+                        4L, statistics.getChild(1).get().getChild(1).get().getValue().get());
+                assertEquals(
+                        4L, statistics.getChild(1).get().getChild(2).get().getValue().get());
+        }
 
-        analyzer.allConfigurations().orElseThrow();
-        analyzer.randomConfigurations(10, 1L).orElseThrow();
-        analyzer.twiseConfigurations(2).orElseThrow();
-    }
+        @SuppressWarnings("unused")
+        @Test
+        public void restOfWrapperTest() {
+                removeTemporaryConstraintsAndSubtree();
+
+                assertTrue(analyzer.isSatisfiable().orElseThrow());
+                assertFalse(analyzer.isVoid().orElseThrow());
+                assertEquals(Arrays.asList("root"), analyzer.core().orElseThrow());
+                assertEquals(
+                        Arrays.asList("altC", "andMandatoryAOptionalA"), analyzer.dead().orElseThrow());
+                assertEquals(
+                        BigInteger.valueOf(54), analyzer.numberOfValidConfigurations().orElseThrow());
+                analyzer.atomicSets().orElseThrow();
+
+                analyzer.project(Arrays.asList("altA", "altC", "orB", "orC")).orElseThrow();
+                analyzer.slice(Arrays.asList("altA", "altC", "orB", "orC")).orElseThrow();
+
+                analyzer.allConfigurations().orElseThrow();
+                analyzer.randomConfigurations(10, 1L).orElseThrow();
+                analyzer.twiseConfigurations(2).orElseThrow();
+        }
+
+        private void removeTemporaryConstraintsAndSubtree() {
+                featureModelBuilder.removeConstraint(removeConstraint2);
+                featureModelBuilder.removeConstraint(removeConstraint1);
+
+                assertTrue(featureModelBuilder.removeFeature(removeremoveA));
+                assertTrue(featureModelBuilder.removeFeatureTree(remove));
+        }
 }
